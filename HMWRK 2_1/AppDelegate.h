cf627dfa593c2e4ce7456bbdbc2ffdcc5e1bb317
Tanechka <smirnovat29-91@mail.ru>
2017-03-01 21:09:26 +0300

    Initial Commit

---
+++ HMWRK 2_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMWRK 2_1
+//
+//  Created by Tanechka on 01.03.17.
+//  Copyright © 2017 Tanechka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMWRK 2_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMWRK 2_1
+//
+//  Created by Tanechka on 01.03.17.
+//  Copyright © 2017 Tanechka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
